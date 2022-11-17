7794b83db7f1d3f8bfe3503cc829119b8f242245
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-17 09:32:49 +0200

    feat: lesson 1 task todo-list

---
+++ lesson1/todo-list/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+const h1 = <h1 className="title">Todo List</h1>;
+
+root.render(h1);
