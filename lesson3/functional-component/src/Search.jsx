87f2a0efba33f437321ecfac5b3d9db73344e698
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-17 16:38:22 +0200

    feat: lesson3 feat class component

--- lesson3/functional-component/src/Search.jsx
+++ lesson3/functional-component/src/Search.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './search.scss';
 
-const Search = prop => {
+const Search = props => {
   return (
     <div className="search">
-      <h1 className="search__title">Hello, {prop.name}. What to search for you?</h1>
+      <h1 className="search__title">Hello, {props.name}. What to search for you?</h1>
       <div className="search__field">
         <input type="text" className="search__input" />
         <button className="search__button">Search</button>
