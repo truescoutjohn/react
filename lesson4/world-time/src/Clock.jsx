60acdd833bf613f9d28d44779a8d2e4451e6d366
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-18 13:55:43 +0200

    fix: change clock component

--- lesson4/world-time/src/Clock.jsx
+++ lesson4/world-time/src/Clock.jsx
@@ -6,6 +6,11 @@ import './clock.scss';
 // 2. Set props to inner state
 // 3. Change and render new time after each second
 //  3.1. Use moment to transform date to time
+const getTimeWithOffset = offset => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+};
 
 class Clock extends Component {
   constructor(props) {
@@ -22,11 +27,7 @@ class Clock extends Component {
       });
     }, 1000);
   }
-  getTimeWithOffset(offset) {
-    const currentTime = new Date();
-    const utcOffset = currentTime.getTimezoneOffset() / 60;
-    return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
-  }
+
   getTime(date) {
     return moment(date).format('h:mm:ss A');
   }
@@ -34,7 +35,7 @@ class Clock extends Component {
     return (
       <div className="clock">
         <div className="clock__location">{this.props.location}</div>
-        <div className="clock__time">{this.getTime(this.getTimeWithOffset(this.props.offset))}</div>
+        <div className="clock__time">{this.getTime(getTimeWithOffset(this.props.offset))}</div>
       </div>
     );
   }
