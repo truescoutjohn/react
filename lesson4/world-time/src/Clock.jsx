ebd3818583c133fbc65ca060603396a41642b410
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-18 14:34:14 +0200

    feat: lesson 4 task world-time

--- lesson4/world-time/src/Clock.jsx
+++ lesson4/world-time/src/Clock.jsx
@@ -6,28 +6,25 @@ import './clock.scss';
 // 2. Set props to inner state
 // 3. Change and render new time after each second
 //  3.1. Use moment to transform date to time
-const getTimeWithOffset = offset => {
-  const currentTime = new Date();
-  const utcOffset = currentTime.getTimezoneOffset() / 60;
-  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
-};
 
 class Clock extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      offset: props.offset,
-      location: props.location,
-      time: this.getTimeWithOffset(),
+      time: this.getTimeWithOffset(this.props.offset),
     };
 
     setInterval(() => {
       this.setState({
-        time: this.getTimeWithOffset(),
+        time: this.getTimeWithOffset(this.props.offset),
       });
     }, 1000);
   }
-
+  getTimeWithOffset(offset) {
+    const currentTime = new Date();
+    const utcOffset = currentTime.getTimezoneOffset() / 60;
+    return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+  }
   getTime(date) {
     return moment(date).format('h:mm:ss A');
   }
@@ -35,7 +32,7 @@ class Clock extends Component {
     return (
       <div className="clock">
         <div className="clock__location">{this.props.location}</div>
-        <div className="clock__time">{this.getTime(getTimeWithOffset(this.props.offset))}</div>
+        <div className="clock__time">{this.getTime(this.getTimeWithOffset(this.props.offset))}</div>
       </div>
     );
   }
