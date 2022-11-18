a2142e41f0c4a6f92005285824a57c6eb2dd5929
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-18 18:23:00 +0200

    feat: lesson5 task class button event

---
+++ lesson5/class-button-event/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import GoodButton from './GoodButton.jsx';
+
+const App = props => {
+  return <GoodButton />;
+};
+
+export default App;
---
+++ lesson5/class-button-event/src/GoodButton.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './goodButton.scss';
+// algo
+// 1. Create class component Clock
+// 2. Set props to inner state
+// 3. Change and render new time after each second
+//  3.1. Use moment to transform date to time
+
+class GoodButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  click(event) {
+    alert(event.target.textContent);
+  }
+
+  render() {
+    return (
+      <div className="fancy-button" onClick={this.click.bind(this)}>
+        Click me
+      </div>
+    );
+  }
+}
+
+export default GoodButton;
