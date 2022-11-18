7d078f5f1ea7848391d6c2f48e3197a3a890abe6
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-18 19:11:28 +0200

    feat: lesson 5 task arguments handler

---
+++ lesson5/arguments-handler/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Colors from './Colors.jsx';
+// algo
+// 1. create function component App
+// 2. insert clock component to app component
+const App = props => {
+  return <Colors />;
+};
+
+export default App;
---
+++ lesson5/arguments-handler/src/Colors.jsx
@@ -0,0 +1,42 @@
+// algo
+// 1. Create class-component Colors
+// 2. Create function which is changed colors
+//   2.1. Get event.target color style
+//   2.2. Apply this color to body
+
+import React, { Component } from 'react';
+
+class Colors extends Component {
+  constructor(props) {
+    super(props);
+    this.switchColor = this.switchColor.bind(this);
+  }
+
+  switchColor(event) {
+    document.body.style.backgroundColor = event.target.style.backgroundColor;
+  }
+
+  render() {
+    return (
+      <div className="counter">
+        <button
+          className="counter__button"
+          style={{ background: '#f00' }}
+          onClick={this.switchColor}
+        ></button>
+        <button
+          className="counter__button"
+          style={{ background: '#0f0' }}
+          onClick={this.switchColor}
+        ></button>
+        <button
+          className="counter__button"
+          style={{ background: '#00f' }}
+          onClick={this.switchColor}
+        ></button>
+      </div>
+    );
+  }
+}
+
+export default Colors;
