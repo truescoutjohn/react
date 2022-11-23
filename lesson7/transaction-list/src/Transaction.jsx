553be23354372c9e4a86cd461f97c3ca360efc2a
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-23 23:50:32 +0200

    fix: change prop on Transaction

--- lesson7/transaction-list/src/Transaction.jsx
+++ lesson7/transaction-list/src/Transaction.jsx
@@ -4,17 +4,17 @@ import moment from 'moment';
 const formatter = new Intl.NumberFormat('en-GB');
 
 const Transaction = props => {
-  const [date, time] = moment(new Date(props.transaction.time)).format('DD MMM-HH:mm').split('-');
+  const [date, time] = moment(new Date(props.time)).format('DD MMM-HH:mm').split('-');
 
   return (
     <li className="transaction">
       <span className="transaction__date">{date}</span>
       <span className="transaction__time">{time}</span>
       <span className="transaction__assets">
-        {props.transaction.from}-{props.transaction.to}
+        {props.from}-{props.to}
       </span>
-      <span className="transaction__rate">{props.transaction.rate}</span>
-      <span className="transaction__amount">{formatter.format(props.transaction.amount)}</span>
+      <span className="transaction__rate">{props.rate}</span>
+      <span className="transaction__amount">{formatter.format(props.amount)}</span>
     </li>
   );
 };
