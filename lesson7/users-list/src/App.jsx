fff2cfc666fdde7fd31d41013aa86359ebb6bdd0
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-23 22:00:39 +0200

    refactor: change components structure

--- lesson7/users-list/src/App.jsx
+++ lesson7/users-list/src/App.jsx
@@ -1,66 +1,31 @@
-import React, { Component } from 'react';
-import UserList from './UserList.jsx';
+import React from 'react';
+import UsersList from './UsersList.jsx';
 
-class App extends Component {
-  state = {
-    sorting: '-',
-  };
+const users = [
+  {
+    id: 'id-0',
+    age: 21,
+    name: 'Bob',
+  },
+  {
+    id: 'id-1',
+    age: 17,
+    name: 'Tom',
+  },
+  {
+    id: 'id-2',
+    age: 18,
+    name: 'Tad',
+  },
+  {
+    id: 'id-3',
+    age: 45,
+    name: 'Sam',
+  },
+];
 
-  users = [
-    {
-      id: 'id-0',
-      age: 21,
-      name: 'Bob',
-    },
-    {
-      id: 'id-1',
-      age: 17,
-      name: 'Tom',
-    },
-    {
-      id: 'id-2',
-      age: 18,
-      name: 'Tad',
-    },
-    {
-      id: 'id-3',
-      age: 45,
-      name: 'Sam',
-    },
-  ];
-
-  changeSorting() {
-    if (this.state.sorting === 'asc') {
-      this.setState({
-        sorting: 'desc',
-        sortingFunc: (a, b) => b.age - a.age,
-      });
-    } else {
-      this.setState({
-        sorting: 'asc',
-        sortingFunc: (a, b) => a.age - b.age,
-      });
-    }
-  }
-
-  render() {
-    let sortedArray = this.users;
-    if (this.state.sorting !== '-') {
-      sortedArray = [...sortedArray].sort(this.state.sortingFunc);
-    }
-    return (
-      <div>
-        <button className="btn" onClick={this.changeSorting.bind(this)}>
-          {this.state.sorting}
-        </button>
-        <ul className="users">
-          {sortedArray.map(user => (
-            <UserList key={user.id} user={user} />
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const App = props => {
+  return <UsersList users={users} />;
+};
 
 export default App;
--- lesson7/users-list/src/User.jsx
+++ lesson7/users-list/src/User.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const User = props => {
   return (
-    <>
+    <li className="user">
       <span className="user__name">{props.name}</span>
       <span className="user__age">{props.age}</span>
-    </>
+    </li>
   );
 };
 
