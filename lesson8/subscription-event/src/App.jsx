1019dda3b7af5bdbfee32d12b63d42749fd8141a
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-25 12:33:14 +0200

    feat: lesson8 task subscription event

---
+++ lesson8/subscription-event/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Dimension from './Dimension.jsx';
+
+function App(props) {
+  return <Dimension />;
+}
+
+export default App;
---
+++ lesson8/subscription-event/src/Dimension.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Dimension extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...this._fillPropObject() };
+    this._resizeEventHandler = this._resizeEventHandler.bind(this);
+    this._fillPropObject = this._fillPropObject.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this._resizeEventHandler);
+    document.title = this.state.title;
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._resizeEventHandler);
+  }
+
+  _resizeEventHandler(event) {
+    this.setState({ ...this._fillPropObject() });
+    document.title = this.state.title;
+  }
+
+  _fillPropObject() {
+    return {
+      innerHeight: window.innerHeight,
+      innerWidth: window.innerWidth,
+      title: `${window.innerHeight} x ${window.innerWidth}`,
+    };
+  }
+  render() {
+    return (
+      <div className="dimensions">
+        {this.state.innerWidth}px - {this.state.innerHeight}px
+      </div>
+    );
+  }
+}
+
+export default Dimension;
