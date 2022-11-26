2125deeb36761c970557108f9759c5389e658dd2
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-26 19:24:14 +0200

    feat: add hide arrow on disable button

--- lesson7/pagination/src/Pagination.jsx
+++ lesson7/pagination/src/Pagination.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+const renderNextButton = (isNextPageAvailable, goNext) => {
+  if (!isNextPageAvailable) {
+    return (
+      <button className="btn" onClick={goNext} disabled={isNextPageAvailable}>
+        →
+      </button>
+    );
+  }
+  return <button className="btn" onClick={goNext} disabled={isNextPageAvailable}></button>;
+};
+
+const renderPrevButton = (isPrevPageAvailable, goPrev) => {
+  if (!isPrevPageAvailable) {
+    return (
+      <button className="btn" onClick={goPrev} disabled={isPrevPageAvailable}>
+        ←
+      </button>
+    );
+  }
+  return <button className="btn" onClick={goPrev} disabled={isPrevPageAvailable}></button>;
+};
 
 const Pagination = props => {
   const isPrevPageAvailable = props.currentPage > 1 ? false : true;
   const isNextPageAvailable =
     props.currentPage < Math.ceil(props.totalItems / props.itemsPerPage) ? false : true;
+  const prevButton = renderPrevButton(isPrevPageAvailable, props.goPrev);
+  const nextButton = renderNextButton(isNextPageAvailable, props.goNext);
+
   return (
     <div className="pagination">
-      <button className="btn" onClick={props.goPrev} disabled={isPrevPageAvailable}>
-        ←
-      </button>
+      {prevButton}
       <span className="pagination__page">{props.currentPage}</span>
-      <button className="btn" onClick={props.goNext} disabled={isNextPageAvailable}>
-        →
-      </button>
+      {nextButton}
     </div>
   );
 };
