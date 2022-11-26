acc5c00d63b20f0b1389f5b6bc1a2e20cdeffaa9
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-26 13:45:05 +0200

    feat: lesson 9 task controlled component

---
+++ lesson9/controlled-component/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Search from './Search.jsx';
+
+function App(props) {
+  return <Search />;
+}
+
+export default App;
---
+++ lesson9/controlled-component/src/Search.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+    this.handlerInput = this.handlerInput.bind(this);
+    this.handlerFormSubmit = this.handlerFormSubmit.bind(this);
+  }
+
+  handlerInput(event) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+  handlerFormSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className="search" onSubmit={this.handlerFormSubmit}>
+        <input
+          type="text"
+          className="search__input"
+          value={this.state.value}
+          onChange={this.handlerInput}
+        />
+        <button type="submit" className="search__button">
+          Search
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Search;
