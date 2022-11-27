dbd7bc8736b022bca9a73c90b36207387aceb192
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-27 23:22:25 +0200

    lesson 9 task tic-tac-toe

---
+++ lesson9/tic-tac-toe/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Game from './Game.jsx';
+import './index.css';
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Game />);
