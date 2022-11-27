a58de215d9b3a8115be91d07c2d68a284c880c4d
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-28 01:00:12 +0200

    feat: lesson 10 task state-lifting

---
+++ lesson10/state-lifting/src/UserProfile.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class UserProfile extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    if (!this.props.userData) {
+      return null;
+    }
+    const { location, name, avatar_url } = this.props.userData;
+    return (
+      <div className="user">
+        <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+        <div className="user__info">
+          <span className="user__name">{name}</span>
+          <span className="user__location">{location}</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
