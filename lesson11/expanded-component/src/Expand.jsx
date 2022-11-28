feb1d77c7eeb69589c83dc9ae1b8feac8098b602
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-28 17:34:34 +0200

    refactor: lesson 11 task Expand

--- lesson11/expanded-component/src/Expand.jsx
+++ lesson11/expanded-component/src/Expand.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Expand = props => {
-  const style = props.isShowed ? {} : { transform: 'rotate(180deg)' };
+  const style = props.isShowed ? { transform: 'rotate(180deg)' } : {};
   return (
     <div className="expand border">
       <div className="expand__header">
         <span className="expand__title">Some title</span>
         <button className="expand__toggle-btn" onClick={props.clickHandler}>
-          <FontAwesomeIcon style={style} icon={faChevronUp} />
+          <i className="fas fa-chevron-up" style={style}></i>
         </button>
       </div>
       {props.isShowed && <div className="expand__content">{props.children}</div>}
