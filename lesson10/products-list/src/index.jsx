d79b2a721942b6c3aa48486aa6380668e3e122df
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-28 10:17:16 +0200

    refactor: change component name

--- lesson10/products-list/src/index.jsx
+++ lesson10/products-list/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import FilterableProductTable from './FilterableProducttable.jsx';
+import FilterableProductTable from './FilterableProductTable.jsx';
 
 const products = [
   { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
