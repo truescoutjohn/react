fbc8ffa81a3f3a11778e9e7c50c3fee2c8deaefc
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-28 03:05:53 +0200

    feat: lesson 10 task calculator temperature

---
+++ lesson10/calculator-temperature/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Calculator from './Calculator.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Calculator />);
