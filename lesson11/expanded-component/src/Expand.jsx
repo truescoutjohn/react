16a4984efa321ee2eb55cf0ad83b1155f2a68c92
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-28 17:43:12 +0200

    refactor: change responsibility of component

--- lesson11/expanded-component/src/Expand.jsx
+++ lesson11/expanded-component/src/Expand.jsx
@@ -1,18 +1,47 @@
 import React from 'react';
 
-const Expand = props => {
-  const style = props.isShowed ? {} : { transform: 'rotate(180deg)' };
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">Some title</span>
-        <button className="expand__toggle-btn" onClick={props.clickHandler}>
-          <i className="fas fa-chevron-up" style={style}></i>
-        </button>
+class Expand extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowed: false,
+    };
+  }
+
+  hide = () => {
+    this.setState({
+      isShowed: false,
+    });
+  };
+
+  show = () => {
+    this.setState({
+      isShowed: true,
+    });
+  };
+
+  clickHandler = event => {
+    if (this.state.isShowed) {
+      this.hide();
+    } else {
+      this.show();
+    }
+  };
+
+  render() {
+    const style = this.state.isShowed ? {} : { transform: 'rotate(180deg)' };
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{this.props.title}</span>
+          <button className="expand__toggle-btn" onClick={this.clickHandler}>
+            <i className="fas fa-chevron-up" style={style}></i>
+          </button>
+        </div>
+        {this.state.isShowed && <div className="expand__content">{this.props.children}</div>}
       </div>
-      {props.isShowed && <div className="expand__content">{props.children}</div>}
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Expand;
