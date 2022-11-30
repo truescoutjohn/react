c31e17a17e5e34c1a7215243ffc02cdd68118230
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-30 17:29:23 +0200

    fix: change class to className'

--- lesson14/counter/src/Counter.jsx
+++ lesson14/counter/src/Counter.jsx
@@ -3,14 +3,14 @@ import React, { useState } from 'react';
 const Counter = props => {
   const [counter, setCounter] = useState(0);
   return (
-    <div class="counter">
-      <button class="counter__button" onClick={() => setCounter(counter - 1)}>
+    <div className="counter">
+      <button className="counter__button" onClick={() => setCounter(counter - 1)}>
         -
       </button>
-      <span class="counter__value" onClick={() => setCounter(0)}>
+      <span className="counter__value" onClick={() => setCounter(0)}>
         {counter}
       </span>
-      <button class="counter__button" onClick={() => setCounter(counter + 1)}>
+      <button className="counter__button" onClick={() => setCounter(counter + 1)}>
         +
       </button>
     </div>
