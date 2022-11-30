69a6cac55a8aa8572695bf3169e4a1d2bde4f1ae
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-30 13:59:27 +0200

    feat: lesson13 task react-router-nested

---
+++ lesson13/react-router-nested/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home.jsx';
+import Products from './Products.jsx';
+
+const App = props => {
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products" component={Products} />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
