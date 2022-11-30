7f36e7f17e49b050222ac921aaf632d6a965fe5d
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-30 16:17:58 +0200

    refactor: setCounter argument

--- lesson14/counter/src/Counter.jsx
+++ lesson14/counter/src/Counter.jsx
@@ -4,13 +4,13 @@ const Counter = props => {
   const [counter, setCounter] = useState(0);
   return (
     <div class="counter">
-      <button class="counter__button" onClick={() => setCounter(prevCounter => prevCounter - 1)}>
+      <button class="counter__button" onClick={() => setCounter(counter - 1)}>
         -
       </button>
       <span class="counter__value" onClick={() => setCounter(0)}>
         {counter}
       </span>
-      <button class="counter__button" onClick={() => setCounter(prevCounter => prevCounter + 1)}>
+      <button class="counter__button" onClick={() => setCounter(counter + 1)}>
         +
       </button>
     </div>
